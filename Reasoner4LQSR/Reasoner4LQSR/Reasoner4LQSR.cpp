ece07af8eb80384df0604869c454241aca941c94
dfsantamaria <santamaria@dmi.unict.it>
2017-05-26 19:51:02 +0200

    test

--- Reasoner4LQSR/Reasoner4LQSR/Reasoner4LQSR.cpp
+++ Reasoner4LQSR/Reasoner4LQSR/Reasoner4LQSR.cpp
@@ -1353,10 +1353,10 @@ int main()
 	*/
 
 	//	insertFormulaKB("( ( V0{l} $EQ V0{x}) $OR  ( V0{l} $QE V0{x})  )", KB);
-	insertFormulaKB("( ( V0{l} $NI V1{C1}) $OR ( ( V0{t} $NI V1{C2}) $OR ( V0{x} $NI V1{C2}) ) )", KB);
-	//insertFormulaKB("( ( V0{l} $NI V1{C1}) $OR  ( V0{x} $NI V1{C2}) ) ", KB);
-	//	insertFormulaKB("( V0{l} $IN V1{C1})", KB);	
-		insertFormulaKB("(V0{ t } $IN V1{ C2 })", KB);
+	//insertFormulaKB("( ( V0{l} $NI V1{C1}) $OR ( ( V0{t} $NI V1{C2}) $OR ( V0{x} $NI V1{C2}) ) )", KB);
+	insertFormulaKB("( ( V0{l} $NI V1{C1}) $OR  ( V0{x} $NI V1{C2}) ) ", KB);
+		insertFormulaKB("( V0{l} $IN V1{C1})", KB);	
+	//	insertFormulaKB("(V0{ t } $IN V1{ C2 })", KB);
 	cout << "---Radix Content ---" << endl;
 	for (int i = 0; i< KB.size(); i++)
 	{
