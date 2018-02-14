f0d03d09f1a8a53a6f772f1497f87d8e8e9dab63
dfsantamaria <santamaria@dmi.unict.it>
2018-02-14 19:51:12 +0100

    test

--- Reasoner4LQSR/Reasoner4LQSR/mainGamma.cpp
+++ Reasoner4LQSR/Reasoner4LQSR/mainGamma.cpp
@@ -158,7 +158,7 @@ int main()
 	vector<int> KB2size(sizeofVVector,0);
 	cout << "Reading OWL File" << endl; 
 	
-	int chk=readOWLXMLOntology("Example/onto1.owl", ontNamespaces, formulae, KB2size);
+	int chk=readOWLXMLOntology("Example/onto2.owl", ontNamespaces, formulae, KB2size);
 	if (chk == -1)
 	{
 		cout << "Ontology not supported" << endl;
