e97f781f5e7048796f2ee86c138a381976c78a30
dfsantamaria <santamaria@dmi.unict.it>
2018-02-26 15:59:10 +0100

    test

--- Reasoner4LQSR/Reasoner4LQSR/tester.cpp
+++ Reasoner4LQSR/Reasoner4LQSR/tester.cpp
@@ -55,7 +55,7 @@ int main()
 	vector<Formula*> KBnorm;
 	vector<Formula*> KBcnf;
 	vector<Formula*> KBmq;
-	string kbname = "Tester/tester2-a.txt";
+	string kbname = "Tester/tester6.txt";
 
 	/*Precomputing Space -- to be implemented*/
 	//  to be implemented
